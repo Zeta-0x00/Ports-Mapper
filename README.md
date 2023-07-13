0177a093df5ce679471e64e57dcff0b343b046ac
Z <zeta.hoffman@protonmail.com>
2023-07-12 22:26:36 -0600

    Mapper
    Simple useful bash tool

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Ports-Mapper
- Little simple bash script to enum machines ports
+# Ports Mapper
+This is a simple tool for scanning ports on other machines in the style of nmap, but written in bash.
+The reason for this tool is for when it is not possible to use nmap for whatever reason in an audit.
+
+# Usage
+
+```bash
+~$ ./mapper.sh <ip>
+```
---
+++ mapper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+handler(){
+  echo -e "\n\nSIGINT detected, exiting..."
+  exit 0
+}
+
+trap handler SIGINT
+
+
+Max=200
+Current=0
+
+#Identify machine OS by TTL
+output=$(ping -c 1 $1 2>/dev/null)
+
+ttl_line=$(echo "$output" | grep -o "ttl=[0-9]*")
+
+ttl=$(echo "$ttl_line" | grep -o "[0-9]*")
+
+if [[ $ttl -le 128 && $ttl -gt 64 ]]; then
+  echo "Posible Windows, TTL = $ttl ."
+elif [[ $ttl -le 64 ]]; then
+  echo "Posible Unix/Linux, TTL = $ttl."
+else
+  echo "OS no reconocido, valor TTL = $ttl."
+fi
+
+
+#ports mapper
+for i in  $(seq 1 65535); do
+  while [[ $Current -ge $Max ]]; do
+    sleep 0.1
+    Current=$(jobs -p | wc -l)
+  done
+
+  {
+    timeout 1 bash -c "echo '' > /dev/tcp/$1/$i" && echo "Puerto $i abierto"
+  } 2>/dev/null &
+  Current=$(jobs -p | wc -l)
+done
+
+wait
