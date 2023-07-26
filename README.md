2ed31be8b0a679af9a27e04a6f3845d8be5d63ba
Z <zeta.hoffman@protonmail.com>
2023-07-25 20:55:27 -0600

    Service Analyser
    - Refactor code
    - Add colors
    - Update README
    - Add service recognition
    - set dependencies nc + ping

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
 # Ports Mapper
-This is a simple tool for scanning ports on other machines in the style of nmap, but written in bash.
-The reason for this tool is for when it is not possible to use nmap for whatever reason in an audit.
+Simple tool for port scanning and port service enumeration.
+
+The reason for this tool is mainly to facilitate moments in security audits where it is known that map cannot be used for various reasons on the network.
+
+The tool depends on 2 binaries on the host system, it needs to have netcat (nc) and ping, in order to proceed with the automation of service identification and the approximation of the possible operating system.
 
 # Usage
 
 ```bash
+~$ chmod +x mapper.sh
 ~$ ./mapper.sh <ip>
+```
+
+# Example of use
+
+
+```haskell
+~$ ./mapper.sh
+[!] Usage:
+	./mapper.sh <ip>
+
+[!] Dependencies:
+	nc
+	ping
+```
+
+```haskell
+
+~$ ./mapper.sh <ip>
+Host Address = <ip>
+
+[✓] Possible Unix/Linux, TTL = 64.
+
+Port 53 -> Open
+Port 80 -> Open
+
+Open Ports =  53,80
+
+[!] Puerto 53 -> (domain) is open
+[!] Puerto 80 -> (http) is open
+
 ```
--- mapper.sh
+++ mapper.sh
@@ -20,10 +20,44 @@ cyan="\e[0;36m\033[1m"
 
 handler(){
   printf "\n\n${red}[X]${end} SIGINT detected\n\texiting...\n"
+  rm "./cache.txt" 2>/dev/null
   exit 0
 }
 
 trap handler SIGINT
+
+fhelp(){
+  printf "${yellow}[!]${end} Usage:\n\t${green}./mapper.sh ${cyan}<ip>${end}\n\n"
+  printf "${yellow}[!]${end} Dependencies:\n\t${purple}nc\n\tping${end}\n"
+}
+
+command_validation(){
+  command -v "$1" > /dev/null
+  if [ ! $? -eq 0 ]; then
+    printf "${red}[X]${end} $1 not exist\n\n"
+    fhelp
+    exit 1
+  fi
+}
+
+if [ $# -ne 1 ]; then
+  fhelp
+  exit 1
+fi
+host="$1"
+
+printf "Host Address = ${cyan}$host${end}\n\n"
+
+command_validation "nc"
+command_validation "ping"
+
+service_identifier(){
+  local lport="$1"
+  local cache=$(nc -zv -w 1 $host $lport 2>&1)
+  local service=$(echo "$cache" | awk '{print $3}')
+  local status=$(echo "$cache" | awk '{print $4}')
+  printf "${yellow}[!]${end} Puerto ${cyan}$lport${end} -> ${purple}$service${end} is ${green}$status${end}\n"
+}
 
 
 Max=200
@@ -36,23 +70,24 @@ ttl_line=$(echo "$output" | grep -o "ttl=[0-9]*")
 
 ttl=$(echo "$ttl_line" | grep -o "[0-9]*")
 if [[ $ttl -eq '' || $ttl -gt 128 ]]; then
-  printf "${yellow}[!]${end} OS no reconocido, valor TTL = ${cyan}$ttl${end}.\n"
+  printf "${yellow}[!]${end} Unrecognised OS, TTL = ${cyan}$ttl${end}.\n\n"
 elif [[ $ttl -le 128 && $ttl -gt 64 ]]; then
-  printf "${green}[✓]${end} Posible Windows, TTL = ${cyan}$ttl${end}.\n"
+  printf "${green}[✓]${end} Possible Windows, TTL = ${cyan}$ttl${end}.\n\n"
 elif [[ $ttl -le 64 ]]; then
-  printf "${green}[✓]${end} Posible Unix/Linux, TTL = ${cyan}$ttl${end}.\n"
+  printf "${green}[✓]${end} Possible Unix/Linux, TTL = ${cyan}$ttl${end}.\n\n"
 fi
 
 #ports mapper
-for i in  $(seq 1 65535); do
+for i in  $(seq 1 100); do #65535
   while [[ $Current -ge $Max ]]; do
     sleep 0.1
     Current=$(jobs -p | wc -l)
   done
 
   {
-    if  timeout 1 bash -c "echo '' > /dev/tcp/$1/$i" 2>/dev/null; then
-      printf "Puerto ${blue}$i${end} -> ${green}abierto${end}\n"
+    if  timeout 1 bash -c "echo '' > /dev/tcp/$host/$i" 2>/dev/null; then
+      echo "$i" >> cache.txt
+      printf "Port ${blue}$i${end} -> ${green}Open${end}\n"
     fi
   }  &
   Current=$(jobs -p | wc -l)
@@ -60,3 +95,12 @@ done
 
 wait
 
+open_ports=$(/bin/cat ./cache.txt | tr "\n" ",")
+rm "./cache.txt"
+open_ports=${open_ports%,}
+
+printf "\nOpen Ports = ${blue} $open_ports ${end}\n\n"
+
+for port in $(echo "$open_ports" | tr ',' ' '); do
+  service_identifier "$port"
+done
